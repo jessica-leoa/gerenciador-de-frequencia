7d52d6735588544dda176d5fd44f0759fa1eb1a5
Lucas-Carvalho-s <Lcs19@cin.ufpe.br>
2025-11-27 15:09:32 -0300

    feat: realizar chamada (ref #14 #15 #16 #17)

--- routes/professor.py
+++ routes/professor.py
@@ -75,5 +75,44 @@ def criar_aula(turma_id):
     db.session.commit()
     
     flash("Aula agendada! Realize a chamada agora.", "success")
-    # Redireciona direto para a chamada
+    
     return redirect(url_for("professor.realizar_chamada", aula_id=nova_aula.id))
+
+@professor_bp.route("/aula/<int:aula_id>/chamada", methods=["GET", "POST"])
+def realizar_chamada(aula_id):
+    aula = Aula.query.get_or_404(aula_id)
+    turma = Turma.query.get(aula.turma_id)
+    alunos = Aluno.query.filter_by(turma_id=turma.id).all()
+    
+    if request.method == "POST":
+       
+        for aluno in alunos:
+            
+            status_presenca = request.form.get(f"presenca_{aluno.id}") == "on"
+            
+            registro = Presenca.query.filter_by(aluno_id=aluno.id, aula_id=aula.id).first()
+            
+            if registro:
+                registro.presenca = status_presenca
+            else:
+                novo_registro = Presenca(
+                    aluno_id=aluno.id,
+                    aula_id=aula.id,  
+                    presenca=status_presenca
+                )
+                db.session.add(novo_registro)
+        
+        db.session.commit()
+        flash("Chamada salva com sucesso!", "success")
+        return redirect(url_for("professor.ver_turma", id=turma.id))
+
+    lista_chamada = []
+    for aluno in alunos:
+        registro = Presenca.query.filter_by(aluno_id=aluno.id, aula_id=aula.id).first()
+        presente = registro.presenca if registro else True 
+        lista_chamada.append({
+            'aluno': aluno,
+            'presente': presente
+        })
+
+    return render_template("chamada.html", aula=aula, turma=turma, lista_chamada=lista_chamada)
