b9a75ec852a73b1d8c69ad676cd084cc1e7f2213
mphumapo2014 <mphumapo10@gmail.com>
2025-11-27 12:10:13 -0300

    feat: adiciona criação de turma (ref #32)

--- routes/professor.py
+++ routes/professor.py
@@ -15,3 +15,16 @@ def listar_turmas():
     turmas = Turma.query.all()
     return render_template("index.html", turmas=turmas)
 
+@professor_bp.route("/turmas/nova", methods=["GET", "POST"])
+def criar_turma():
+    if request.method == "POST":
+        nome = request.form.get("nome")
+        carga_horaria = request.form.get("carga_horaria")
+        
+        nova_turma = Turma(nome=nome, carga_horaria=carga_horaria)
+        db.session.add(nova_turma)
+        db.session.commit()
+        flash("Turma criada com sucesso!", "success")
+        return redirect(url_for("professor.listar_turmas"))
+    
+    return render_template("turma_form.html")
