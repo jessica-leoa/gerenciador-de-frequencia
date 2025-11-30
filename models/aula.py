50cd2e7e1233bb9ae076b8a87c65edb5bc33c353
Tobias <tobias.rdso@gmail.com>
2025-11-29 23:11:07 -0300

    fix: atualização do modelo de aula (ref #51)

--- models/aula.py
+++ models/aula.py
@@ -1,11 +1,17 @@
 from extensions import db
+from datetime import date
 
 class Aula(db.Model):
     __tablename__ = "aulas"
 
     id = db.Column(db.Integer, primary_key=True)
-    turma_id = db.Column(db.Integer, db.ForeignKey("turmas.id"), nullable=False)
-    data_aula = db.Column(db.Date, nullable=False)
+    data_aula = db.Column(db.Date, default=date.today, nullable=False)
+    conteudo = db.Column(db.String(100))
+    turma_id = db.Column(db.Integer, db.ForeignKey('turmas.id', ondelete='CASCADE'), nullable=False)
     
-    # Relacionamento com Turma
-    turma = db.relationship("Turma", backref="aulas")
+    # Relacionamentos
+    turma = db.relationship('Turma', backref=db.backref('aulas', lazy=True, cascade="all, delete-orphan"))
+    presencas = db.relationship('Presenca', backref='aula', lazy=True, cascade="all, delete-orphan")
+
+    def __repr__(self):
+        return f'<Aula {self.data_aula.strftime("%d/%m/%Y")} - {self.turma_id}>'
